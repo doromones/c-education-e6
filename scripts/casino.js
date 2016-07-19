12734ded561ee11c8f00cf4b5427ba0f6da7ff68
Антон Радушев <doromones@gmail.com>
2016-07-19 15:07:12 +0000

    .

--- scripts/casino.js
+++ scripts/casino.js
@@ -3,7 +3,7 @@
 function SlotMachine(money) {
   money = Number(money);
   var putedMoney = 0;
-  
+
   this.putMoney = function(money){
     if (isNaN(money) || money <= 0) {
       throw '[SlotMachine] Start money must be greater 0';
@@ -11,18 +11,17 @@ function SlotMachine(money) {
       putedMoney += money;
     }
   };
-  
+
   this.getWinnerMoney = function(){
     var money  = putedMoney;
     putedMoney = 0;
     return money;
   };
 
-  this.showBank = function(){
-    console.log('Bank is %i; you put %i', money, putedMoney);
+  this.getBankData = function(){
     return {money: money, putedMoney: putedMoney};
   };
-  
+
   this.play = function(playSum){
     if (money === 0) {
       throw '[SlotMachine] I\'m sorry, I empty. Select another machine.'
@@ -86,63 +85,88 @@ function SlotMachine(money) {
       } else {
         console.log('You lose :(')
       }
-      this.showBank()
     }
   }
-  
+
   return {
     machine: this, 
-    destroy: function(){
-      if (money > 0) {
-        throw '[SlotMachine] Money exist in machine, please get money';
-      }
-      if (putedMoney > 0) {
-        throw '[SlotMachine] Putted Money exist in machine, please get money';
-      }
-      delete this;
-    },
     getMoney: function(){
       var _money = money;
       money = 0;
       return _money;
+    },
+    addMoney: function(_money){
+      _money = Number(_money);
+      if (isNaN(_money) || _money <= 0) {
+        throw '[SlotMachine] Money must be greater 0';
+      }
+      money += _money;
     }
   };
 }
 
 function Casino(slotMachinesCount, money) {
   var slotMachines = [];
-  
+
   slotMachinesCount = Number(slotMachinesCount);
   if (isNaN(slotMachinesCount) || slotMachinesCount <= 0) {
     throw '[Casino] Slot Machines count must be greater 0';
   }
-  
+
   money = Number(money);
   if (isNaN(money) || money <= 0) {
     throw '[Casino] Start money must be greater 0';
   }
-  
-  this.getSlotMachinesCount = function() {
-    return slotMachines.length;
-  };
-  
-  this.getSlotMachine = function(index){
-    var machine = slotMachines[index]['machine'];
+
+  function _getSlotMachine(index){
+    var machine = slotMachines[index];
     if (machine === undefined){
       throw '[Casino] slotMachine not found';
     }
     return machine;
+  }
+
+  function _deleteSlotMachine(index){
+    slotMachines.splice(index, 1);
+  }
+  /* отладочная фигня, закоментить*/
+  this.getRawMachine = function(index){
+    return _getSlotMachine(index);
   };
-  
+
+  this.getSlotMachinesCount = function() {
+    return slotMachines.length;
+  };
+
+  this.getSlotMachine = function(index){
+    var machine = _getSlotMachine(index)['machine'];
+    return machine;
+  };
+
+  this.destroySlotMachine = function(index) {
+    var machine = _getSlotMachine(index);
+    var money = machine.getMoney();
+
+    _deleteSlotMachine(index)
+    var _length = this.getSlotMachinesCount();
+    var mainMoney = Math.round(money / _length);
+    var remainderOfDivision = money % _length;
+
+    for (var i = 0; i < _length; i++) {
+      var slotMachine = _getSlotMachine(i);
+      slotMachine.addMoney(i === 0 ? (mainMoney + remainderOfDivision) : mainMoney);
+    }
+  };
+
   this.printBank = function(){
     var result = {};
     slotMachines.forEach(function(slotMachine, index){
-      console.log('in %i slotMachine %i money', index, slotMachine.machine.showBank.money);
-      result[index] = slotMachine.machine.showBank.money;
+      console.log('in %i slotMachine %i money', index, slotMachine.machine.getBankData().money);
+      result[index] = slotMachine.machine.getBankData().money;
     });
     return result;
   };
-  
+
   (function() {
     var mainMoney = Math.round(money / slotMachinesCount);
     var remainderOfDivision = money % slotMachinesCount;
