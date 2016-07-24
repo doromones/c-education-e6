e1d9c0ad70f29dec784c646731a2d03ac08a203d
doromones <doromones@gmail.com>
2016-07-24 16:14:01 +0300

    Добавляю хендлер ошибок

--- scripts/decorators/exception_handler.js
+++ scripts/decorators/exception_handler.js
@@ -2,7 +2,6 @@ app.config(function($provide) {
     $provide.decorator("$exceptionHandler", ['$delegate', function($delegate) {
         return function(exception, cause) {
             $delegate(exception, cause);
-            debugger
             alert(exception.message);
         };
     }]);
