685245073ceb55f6da4ae8f60245201062ed33e3
Anton Radushev <a.radushev@ticketstravelnetwork.net>
2016-07-25 16:18:41 +0300

    подключаю директиву

--- scripts/directives/rotator.js
+++ scripts/directives/rotator.js
@@ -6,6 +6,32 @@ app.directive('rotator', function () {
         templateUrl: 'rotator.html',
         scope: {
             number: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+            window.ELEMENT = $element;
+            var $rotators = $element.find('.rotator');
+            var $first_wheel = angular.element($rotators[0]);
+            var $second_wheel = angular.element($rotators[1]);
+            var $third_wheel = angular.element($rotators[2]);
+
+            // startRotating($first_wheel, 1000);
+            startRotating($second_wheel, 1500);
+            // startRotating($third_wheel, 2000);
+
+            function startRotating($wheel, time) {
+                var increment = 0;
+                return setInterval(function () {
+                    increment++;
+                    $wheel.css({
+                        '-webkit-transform': 'rotateX(' + (increment * -36) + 'deg)'
+                    });
+                    var pastSix = 0;
+                    if (increment > 9) {
+                        increment
+                    }
+                    $wheel.attr('data-state', increment % 10);
+                }, time || 1000);
+            }
         }
     }
 });
